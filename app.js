851a7587ac5e29822f3ccf9a4c2dcb62d3eef2ef
Jevgeni van Steenis <jevgenivansteenis@gmail.com>
2018-08-15 17:13:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+// JS for height of main nav
+const navHeight = document.querySelector('#nav-container').offsetHeight;
+const targetNavBar = document.querySelector('nav');
+targetNavBar.style.height = navHeight + 'px';
+
+// JS For Reveal JS
+var slideUp = {
+  distance: '150%',
+  origin: 'bottom',
+  opacity: null
+};
+
+var slideLeft = {
+  distance: '150%',
+  origin: 'left',
+  opacity: null
+}
+
+ScrollReveal().reveal('.main-title', {duration: 1000, delay: 250});
+ScrollReveal().reveal('.main-subtitle', {duration: 1000, delay: 1000});
+
+ScrollReveal().reveal('#about', {duration: 500});
+ScrollReveal().reveal('.about-left-side', slideLeft, {duration: 500, delay: 250});
+ScrollReveal().reveal('.about-right-side', slideLeft, {duration: 500, delay: 250});
+
+ScrollReveal().reveal('#portfolio', {duration: 500});
+ScrollReveal().reveal('.portfolio-title', {duration: 750, delay: 250});
+ScrollReveal().reveal('.portfolio-desc', {duration: 750, delay: 250});
+ScrollReveal().reveal('.portfolio-card', slideUp, {duration: 750, delay: 500});
+
+ScrollReveal().reveal('#skills', {duration: 500});
+ScrollReveal().reveal('.skills-title', {duration: 750, delay: 250});
+ScrollReveal().reveal('.skills-desc', {duration: 750, delay: 250});
+ScrollReveal().reveal('.skill', slideLeft, {duration: 750, delay: 1000});
+
+//Smooth Scroll
+var scroll = new SmoothScroll('a[href*="#"]');
+
+//Adding active class based on active position
+const mainSection = document.querySelector('#header');
+const aboutSection = document.querySelector('#about');
+const portfolioSection = document.querySelector('#portfolio');
+const skillsSection = document.querySelector('#skills');
+
+const aboutSectionOffset = aboutSection.offsetHeight - 100;
+const portfolioSectionOffset = portfolioSection.offsetHeight - 100;
+const skillsSectionOffset = skillsSection.offsetHeight - 100;
+
+const mainSectionBtn = document.querySelector('.home-btn');
+const aboutSectionBtn = document.querySelector('.about-btn');
+const portfolioSectionBtn = document.querySelector('.portfolio-btn');
+const skillsSectionBtn = document.querySelector('.skills-btn');
+
+console.log(mainSection);
+console.log(aboutSection);
+console.log(portfolioSection);
+console.log(skillsSection);
+
+window.onscroll = function() {
+  console.log(window.pageYOffset);
+  if(window.pageYOffset < aboutSection.offsetTop) {
+    mainSectionBtn.classList.add('active');
+    aboutSectionBtn.classList.remove('active');
+    portfolioSectionBtn.classList.remove('active');
+    skillsSectionBtn.classList.remove('active');
+  } else if(window.pageYOffset >= aboutSection.offsetTop && window.pageYOffset < portfolioSection.offsetTop) {
+    aboutSectionBtn.classList.add('active');
+    mainSectionBtn.classList.remove('active');
+    portfolioSectionBtn.classList.remove('active');
+    skillsSectionBtn.classList.remove('active');
+  } else if(window.pageYOffset >= portfolioSection.offsetTop && window.pageYOffset < skillsSection.offsetTop) {
+    aboutSectionBtn.classList.remove('active');
+    mainSectionBtn.classList.remove('active');
+    portfolioSectionBtn.classList.add('active');
+    skillsSectionBtn.classList.remove('active');
+  } else if(window.pageYOffset >= skillsSection.offsetTop) {
+    aboutSectionBtn.classList.remove('active');
+    mainSectionBtn.classList.remove('active');
+    portfolioSectionBtn.classList.remove('active');
+    skillsSectionBtn.classList.add('active');
+  }
+}
---
+++ typed.js
@@ -0,0 +1,9 @@
+var mainTitleTyped = new Typed('.typed-main-title', {
+  strings: ["Hey! Mijn naam is Jevgeni!"],
+  typeSpeed: 40
+});
+
+var subTitleTyped = new Typed('.typed-main-subtitle', {
+  strings:["^2000 Welkom op mijn portfolio website"],
+  typeSpeed: 40
+});
